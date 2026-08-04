e61650efe5ac287fdb3072d30185c7cf4ac9b9f2
mhabishkhadka123 <mabhish1234@gmail.com>
2026-08-04 12:55:39 +0545

    index backend/src

--- backend/src/index.js
+++ backend/src/index.js
@@ -84,6 +84,9 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+server.keepAliveTimeout = 120000;
+server.headersTimeout = 125000;
+
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV}`);
